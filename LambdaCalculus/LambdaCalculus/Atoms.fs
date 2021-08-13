95d7a6d1c1e6de5ea15b3d5d6772bc525362faeb
WhiteBlackGoose <wbg@angouri.org>
2021-08-13 23:05:24 +0300

    Update Atoms.fs

--- LambdaCalculus/LambdaCalculus/Atoms.fs
+++ LambdaCalculus/LambdaCalculus/Atoms.fs
@@ -11,11 +11,14 @@ type Expression =
 let rec substitute x value = function
     | Variable x' when x' = x -> value
     | Constant c -> Constant c
-    | Lambda (y, body) when y <> x -> substitute x value body
+    | Lambda (y, body) when y <> x -> Lambda(y, substitute x value body)
     | Applied (lambda, argument) -> Applied(substitute x value lambda, substitute x value argument)
     | other -> other
 
 
 let rec betaReduce = function
-    | Applied(Lambda(x, body), arg) -> substitute x arg body
+    | Applied(maybeLambda, arg) -> 
+        match betaReduce maybeLambda with
+        | Lambda(x, body) -> substitute x arg body
+        | other -> Applied(other, arg)
     | other -> other
