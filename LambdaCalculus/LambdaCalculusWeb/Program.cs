90c117b92aeea0112b244db00b98a68006ef21cd
WhiteBlackGoose <wbg@angouri.org>
2021-08-14 12:30:46 +0300

    Web app

---
+++ LambdaCalculus/LambdaCalculusWeb/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using LambdaCalculus;
+
+
+namespace LambdaCalculusWeb
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var p = Parsing.parse("");
+
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
